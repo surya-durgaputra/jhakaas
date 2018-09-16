7bff1b4057038f2b42d5a425558be99eb8eb10eb
surya-durgaputra <surya.durgaputra@gmail.com>
2018-09-16 17:39:53 +0400

    latest

--- jhakaas-fe/src/components/forms/NewTicket.js
+++ jhakaas-fe/src/components/forms/NewTicket.js
@@ -1,51 +1,71 @@
 import React from 'react';
 import './NewTicket.css'
 const NewTicket = (props) => {
+    let ticket = props.ticket
     return ( 
         <form className="form">
             <div className="form-group form-row">
                 <div className="form-group col-md-6">
-                    <label htmlFor="ToTicket" className="col-form-label sr-only">To Email</label>
-                    <input type="email" className="form-control" id="ToTicket" aria-describedby="emailHelp" placeholder="To Email"/>
+                    <label htmlFor="ToTicket" 
+                     className={"col-form-label " + (ticket==null?"sr-only":"label-left-align")}>
+                     To:</label>
+                    <input type="email" className="form-control" 
+                     id="ToTicket" aria-describedby="emailHelp" 
+                     placeholder="To Email"
+                     value={ticket!=null?ticket.to:""}/>
                 </div>
                 <div className="form-group col-md-6">
-                    <label htmlFor="FromTicket" className="col-form-label sr-only">From address</label>
-                    <input type="email" className="form-control" id="FromTicket" aria-describedby="emailHelp" placeholder="From Email"/>
+                    <label htmlFor="FromTicket" 
+                    className={"col-form-label " + (ticket==null?"sr-only":"label-left-align")}>
+                    From:</label>
+                    <input type="email" className="form-control" 
+                     id="FromTicket" aria-describedby="emailHelp" 
+                     placeholder="From Email"
+                     value={ticket!=null?ticket.from:""}/>
                 </div>
             </div>
             
             <div className="form-group">
-                <label htmlFor="SubjectTicket" className="sr-only">Subject</label>
-                <input type="text" className="form-control" id="SubjectTicket" aria-describedby="subject" placeholder="Subject"/>
+                <label htmlFor="SubjectTicket" 
+                 className={"col-form-label " + (ticket==null?"sr-only":"label-left-align")}>
+                 Subject:</label>
+                <input type="text" className="form-control" id="SubjectTicket" 
+                 aria-describedby="subject" placeholder="Subject"
+                 value={ticket!=null?ticket.subject:""}/>
             </div>
-            <div className="form-group clearfix">
-                <label htmlFor="TicketDescription" className="ticket-description-label float-left sr-only">Description</label>
-                <textarea className="form-control rounded-0" id="TicketDescription" rows="10" aria-describedby="Ticket Description" placeholder="Description of Ticket..."></textarea>
+            <div className="form-group">
+                <label htmlFor="TicketDescription" 
+                 className={"ticket-description-label " + (ticket==null?"sr-only":"label-left-align")}>
+                 Description</label>
+                <textarea className="form-control rounded-0" id="TicketDescription" 
+                 rows="10" aria-describedby="Ticket Description" 
+                 value={ticket!=null?ticket.description:""}
+                 placeholder="Description of Ticket..."/>
             </div>
             <div className="form-group form-row">
                 <div className="form-group col-md-2 offset-md-1">
-                    <select className="custom-select" defaultValue="0">
+                    <select className="custom-select" defaultValue={ticket!=null?ticket.department:"0"}>
                         <option value="0">Department</option>
                         <option value="1">Operations</option>
                         <option value="2">R&amp;D</option>
                     </select>
                 </div>
                 <div className="form-group col-md-2">
-                    <select className="custom-select" defaultValue="0">
+                    <select className="custom-select" defaultValue={ticket!=null?ticket.channel:"0"}>
                         <option value="0">Channel</option>
                         <option value="1">Operations</option>
                         <option value="2">R&amp;D</option>
                     </select>
                 </div>
                 <div className="form-group col-md-2">
-                    <select className="custom-select" defaultValue="0">
+                    <select className="custom-select" defaultValue={ticket!=null?ticket.priority:"0"}>
                         <option value="0">Priority</option>
                         <option value="1">Operations</option>
                         <option value="2">R&amp;D</option>
                     </select>
                 </div>
-                <div className="form-group col-md-2" defaultValue="0">
-                    <select className="custom-select">
+                <div className="form-group col-md-2">
+                    <select className="custom-select"  defaultValue={ticket!=null?ticket.type:"0"}>
                         <option value="0">Type</option>
                         <option value="1">Operations</option>
                         <option value="2">R&amp;D</option>
--- jhakaas-fe/src/components/forms/Tickets.js
+++ jhakaas-fe/src/components/forms/Tickets.js
@@ -1,46 +1,23 @@
 import React, { Component } from 'react';
-
-// const Tickets = ({tickets}) => {
-//     return ( 
-//         <div className="row">
-//             <div className="col-md-2">
-//                 <div className="d-flex flex-column">
-//                     {
-//                         tickets.map(item=>(
-//                             <div className="bg-warning" key={item.id}>
-//                                 {item.subject}
-//                             </div>
-//                         ))
-//                     }
-//                 </div>
-//             </div>
-//             <div className="col-md-10">
-// ticket description
-//             </div>
-//         </div>
-            
-//     );
-// }
- 
-// export default Tickets;
-
+import NewTicket from './NewTicket'
+import './Tickets.css'
 class Tickets extends Component {
     constructor(props) {
         super(props);
         this.state = { item:props.tickets[0] }
     }
-    handleClick = e =>{
-console.log("clicked")
+    handleClick = (key) =>{
+        this.setState({...this.state,item:this.props.tickets[key]})
     }
     render() { 
         return ( 
             <div className="row">
                 <div className="col-md-2">
-                    <div className="d-flex flex-column">
+                    <div className="d-flex flex-column list-group tickets">
                         {
                             this.props.tickets.map(item=>(
-                                <div className="bg-warning" key={item.id} 
-                                    onClick={(e)=>{this.handleClick(e)}}>
+                                <div className="t-item" key={item.id} 
+                                    onClick={(e)=>{this.handleClick(item.id)}}>
                                         {item.subject}
                                 </div>
                             ))
@@ -48,17 +25,7 @@ console.log("clicked")
                     </div>
                 </div>
                 <div className="col-md-10">
-                    <div className="d-flex flex-column">
-                        <div className="row">
-                            <div className="col-md-6">
-{this.state.item.subject}
-                            </div>
-                            <div className="col-md-6">
-                            
-                            </div>
-                        </div>
-                    </div> 
-                    
+                    <NewTicket ticket={this.state.item} />
                 </div>
             </div>
                 
